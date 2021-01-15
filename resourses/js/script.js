30bc11f9bc02d473ecd9d6a200a52671fa730978
with0p <with2p@yandex.ru>
2021-01-15 20:30:23 +0300

    Scroll to ancors

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-    // $('nav').addClass('sticky')
-
+    // sticky nav
     $('.js--section-features').waypoint(function(direction) {
         if (direction == 'down') {
             $('nav').addClass('sticky')
@@ -10,6 +9,15 @@ $(document).ready(function() {
 
         }
     });
+
+    //scroll on buttons
+    $('.js--scroll-to-plan').click(function() {
+        $('html,body').animate({ scrollTop: $('.js--section-plan').offset().top }, 500);
+    });
+    $('.js--scroll-to-next').click(function() {
+        $('html').animate({ scrollTop: $('.js--section-features').offset().top }, 500);
+    });
+    $('html,body').css('scroll-behavior', 'smooth');
 
 
 });
