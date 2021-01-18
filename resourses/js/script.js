4b81e92c6a9cb3499a9f2223e8cae03d27bc9021
with0p <with2p@yandex.ru>
2021-01-18 12:53:27 +0300

    Navigation buttons

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -47,4 +47,22 @@ $(document).ready(function() {
     });
 
 
+    // Navigation buttons
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i')
+        nav.slideToggle(100);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+
+
+    })
+
+
 });
